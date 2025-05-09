2f6472b5aefaaaead4a973f0bce689afc73ea8fd
DevanshMishra123 <mishra24devansh@gmail.com>
2025-05-10 02:37:37 +0530

    changed files

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -16,6 +16,7 @@ export default function ChatClient() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState(session?.user?.messages || []);
   const [error, setError] = useState("");
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     if (status === "authenticated") {
@@ -54,6 +55,17 @@ export default function ChatClient() {
     if (session?.user?.messages?.length) {
       setMessages(session.user.messages);
     }
+    const getUsers = async () => {
+      try {
+        const res = await fetch("/api/users");
+        const data = await res.json();
+        const usernames = data.usernames;
+        setUsers(usernames);
+      } catch (error) {
+        console.error("Failed to save messages:", error);
+      }
+    };
+    getUsers();
   }, [session]);
 
   const sendMessage = async () => {
@@ -93,49 +105,54 @@ export default function ChatClient() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center pt-8">
-      {error && (
-        <div className="absolute top-6 right-6 bg-red-500 text-white px-4 py-2 rounded shadow-md animate-pulse z-50">
-          {error}
-        </div>
-      )}
-      <div className="w-[70vw] h-[80vh] bg-white/10 backdrop-blur-md rounded-2xl border border-white/20 shadow-lg flex flex-col overflow-hidden">
-        <div className="flex-1 overflow-y-auto p-4 space-y-2">
-          {messages.map((msg, idx) => (
-            <div
-              key={idx}
-              className={`w-full flex ${
-                msg.type === 0 ? "justify-end" : "justify-start"
-              }`}
-            >
+    <div className="flex justify-between">
+      <div className="bg-black w-[150px]">{users.map((user,idx)=><div className="bg-black text-white py-4 px-4">{user}</div>)}</div>
+      <div className="flex flex-col items-center justify-center pt-8 px-16">
+        {error && (
+          <div className="absolute top-6 right-6 bg-red-500 text-white px-4 py-2 rounded shadow-md animate-pulse z-50">
+            {error}
+          </div>
+        )}
+        <div className="w-[70vw] h-[80vh] bg-white/10 backdrop-blur-md rounded-2xl border border-white/20 shadow-lg flex flex-col overflow-hidden">
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {messages.map((msg, idx) => (
               <div
-                className={`p-2 rounded-md max-w-[80%] text-white ${
-                  msg.type === 0 ? "bg-blue-500" : "bg-gray-500"
+                key={idx}
+                className={`w-full flex ${
+                  msg.type === 0 ? "justify-end" : "justify-start"
                 }`}
               >
-                {msg.name && msg.type === 1 && (
-                  <p className="text-[11px] text-blue-200 mb-1">~{msg.name}</p>
-                )}
-                <p>{msg.message}</p>
+                <div
+                  className={`p-2 rounded-md max-w-[80%] text-white ${
+                    msg.type === 0 ? "bg-blue-500" : "bg-gray-500"
+                  }`}
+                >
+                  {msg.name && msg.type === 1 && (
+                    <p className="text-[11px] text-blue-200 mb-1">
+                      ~{msg.name}
+                    </p>
+                  )}
+                  <p>{msg.message}</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className="flex items-center gap-3 p-4 border-t border-white/10 bg-white/5 backdrop-blur-sm">
-          <Input
-            id="name"
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder="Enter your message"
-            className="flex-1 text-white bg-white/10 backdrop-blur-md placeholder-white/50 p-2 rounded"
-          />
-          <Button
-            onClick={sendMessage}
-            className="bg-blue-600 text-white px-4 py-2 rounded"
-          >
-            Send
-          </Button>
+            ))}
+          </div>
+          <div className="flex items-center gap-3 p-4 border-t border-white/10 bg-white/5 backdrop-blur-sm">
+            <Input
+              id="name"
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Enter your message"
+              className="flex-1 text-white bg-white/10 backdrop-blur-md placeholder-white/50 p-2 rounded"
+            />
+            <Button
+              onClick={sendMessage}
+              className="bg-blue-600 text-white px-4 py-2 rounded"
+            >
+              Send
+            </Button>
+          </div>
         </div>
       </div>
     </div>
