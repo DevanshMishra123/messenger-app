4e10cb63b685899e7d3cc45164ef16747cb81604
DevanshMishra123 <mishra24devansh@gmail.com>
2025-05-12 14:39:47 +0530

    changed files

--- app/api/create-room/route.js
+++ app/api/create-room/route.js
@@ -1,7 +1,7 @@
 import dbConnect from '../../../lib/mongodb';
 import bcrypt from "bcryptjs";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions"; 
+import { authOptions } from "../../auth/[...nextauth]/authOptions"; 
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
